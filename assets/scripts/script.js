77616d0f27f429ccca583d044a71124b768281cd
regissanme <rsanme@gmail.com>
2022-06-28 00:38:36 -0300

    create script for home page

---
+++ assets/scripts/script.js
@@ -0,0 +1,38 @@
+function handleMouseEnter() {
+    this.classList.add('p-card--hovered');
+    document.body.id = `${this.id}-hovered`;
+    console.log('mouse entered...');
+}
+
+function handleMouseLeave() {
+    this.classList.remove('p-card--hovered');
+    document.body.id = '';
+    console.log('mouse leaved...')
+}
+
+function addEventListenersToCards() {
+    const cardElements = document.getElementsByClassName('p-card');
+
+    for (let index = 0; index < cardElements.length; index++) {
+        const card = cardElements[index];
+        card.addEventListener('mouseenter', handleMouseEnter);
+        card.addEventListener('mouseleave', handleMouseLeave);        
+    }
+}
+
+document.addEventListener("DOMContentLoaded", addEventListenersToCards, false);
+
+function selectCarouselItem(selectedButtonElement) {
+    const selectedItem = selectedButtonElement.id;
+    const carousel = document.querySelector('.p-cards-carousel');
+    const transform = carousel.style.transform;
+    const rotateY = transform.match(/rotateY\((-?\d+deg)\)/i);
+    const rotateYDeg = -120 * (Number(selectedItem) - 1);
+    const newTransform = transform.replace(rotateY[0], `rotateY(${rotateYDeg}deg)`);
+
+    carousel.style.transform = newTransform;
+
+    const activeButtonElement = document.querySelector('.p-controller__button--active');
+    activeButtonElement.classList.remove('p-controller__button--active');
+    selectedButtonElement.classList.add('p-controller__button--active');
+}
